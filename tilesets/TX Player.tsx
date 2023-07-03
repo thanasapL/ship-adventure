2efdaede5253a89ccce3fb48a3ffc100e5a91dbc
thanasapL <thanasap.lee@outlook.com>
2023-07-03 21:21:16 +0700

    [Update] new map

---
+++ tilesets/TX Player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="TX Player" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../../../Downloads/Pixel Art Top Down - Basic/Texture/TX Player.png" width="128" height="128"/>
+</tileset>
